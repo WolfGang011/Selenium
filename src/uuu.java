2ee450d1f8f2eb998885433afc0cb6266404c198
WoftGang011 <thaiphong011199@gmail.com>
2025-01-09 16:28:16 +0700

    test

--- src/uuu.java
+++ src/uuu.java
@@ -1,5 +1,9 @@
 public class uuu {
     public static void main(String[] args){
         System.out.println("Test");
+        System.out.println("Test1");
+        System.out.println("Tes2t");
+        System.out.println("Test3");
+
     }
 }
